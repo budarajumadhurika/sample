cf2d89a4a2ae24abac8e07a43e57cbe382a6db06
madhurika,budaraju <budaraju.madhurika@gmail.com>
2022-04-30 14:36:03 +0530

    first commit

---
+++ src/main/java/org/teamdemo/sample/App.java
@@ -0,0 +1,13 @@
+package org.teamdemo.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "DEMO ON MAVEN" );
+    }
+}
